c21ebf11a68734d055db8d65efaeda6f92ed9557
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-25 13:05:14 +0800

    first commit

--- routes/calculator.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var calculatorController = require("../controllers/calculatorController")
-
-// DEFINING ENDPOINTS
-router.get('/add', (req, res) => {console.log(req.query)
-res.send(req.query)})
-
-module.exports = router;
